ca6b957c1174d3c084548f986795cb8ff520d05a
Olowonyo Olawale <31905212+Olawale4444@users.noreply.github.com>
2020-01-07 23:39:23 +0100

    Add files via upload

---
+++ longestStringinArr.js
@@ -0,0 +1,37 @@
+function longestStr(arr){
+    
+//takes in the first two words in the array, compares and switches them,
+//then continues through the array in the same fashion
+
+    return arr.reduce((firstWord, nextWord)=> {
+        console.log("firstWord is ", firstWord) 
+        console.log("nextWord is", nextWord)
+        return firstWord.length >nextWord.length ? firstWord:nextWord;
+    });
+}
+//if the longest words are of equal length, 
+//it returns the longest last item in the array 
+
+
+//test cases
+longestStr(['ola', 'wiley', 'Jamie', 'Santa', 'Scott', ''])
+
+
+
+//Second Solution
+
+function longestStringInArr(arr){
+	var longestValue = 0;
+	var longestString = "";
+
+	for (let i = 0; i < arr.length ; i++){
+
+		if (arr[i].length > longestValue){
+			arr[i].length = longestValue;
+			 longestString = arr[i];
+		}
+	}
+	return longestString;
+}
+
+longestStr(['ola', 'Hola', 'Ed', "Wiley", 'ddouble', 'presidentT'])
